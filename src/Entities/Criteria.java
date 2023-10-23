909eed826422d008a54169c677392d992eb41772
Yam Wachiralappaitoon <yam.wachiralappaitoon@mail.utoronto.ca>
2023-10-23 12:25:32 -0400

    created Criteria entity

---
+++ src/Entities/Criteria.java
@@ -0,0 +1,9 @@
+package Entities;
+import java.util.List;
+
+public class Criteria {
+    private int calorieGoal;
+    private List<String> dietaryRestrictions;
+    private List<String> ingredientsAvailable;
+    private int prepTime;
+}
