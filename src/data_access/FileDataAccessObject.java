791e7787b9246a3ba21840c47da48628b5614b99
yimanxu <yiman.xu@mail.utoronto.ca>
2023-11-25 21:51:50 -0500

    added FileDataAccessObject that implements MealPlanDataAccessInterface, enable saveMealPlan and loadMealPlan

---
+++ src/data_access/FileDataAccessObject.java
@@ -0,0 +1,35 @@
+package data_access;
+
+import Entities.MealPlan;
+import UseCase.MealPlanCreation.MealPlanDataAccessInterface;
+
+import java.io.File;
+import java.io.IOException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+public class FileDataAccessObject implements MealPlanDataAccessInterface {
+    private final ObjectMapper objectMapper;
+    private final String directoryPath;
+
+    public FileDataAccessObject(String directoryPath) {
+        this.directoryPath = directoryPath;
+        this.objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+    }
+
+    @Override
+    public void saveMealPlan(MealPlan mealPlan) throws IOException {
+        File file = new File(directoryPath, mealPlan.getIdentifier() + ".json");
+        objectMapper.writeValue(file, mealPlan);
+    }
+
+    @Override
+    public MealPlan loadMealPlan(String identifier) throws IOException {
+        File file = new File(directoryPath, identifier + ".json");
+        if (!file.exists()) {
+            return null;
+        }
+        return objectMapper.readValue(file, MealPlan.class);
+    }
+
+}
