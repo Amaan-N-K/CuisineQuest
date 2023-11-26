2c5e791f7c3dfd23a6a55cee02dabd3300417db8
yimanxu <yiman.xu@mail.utoronto.ca>
2023-11-25 21:54:14 -0500

    added MealPlanController, MealPlanPresenter, MealPlanViewModel, MealPlanState, ViewModel and ViewManagerModel

---
+++ src/InterfaceAdapter/MealPlanCreation/MealPlanState.java
@@ -0,0 +1,94 @@
+package InterfaceAdapter.MealPlanCreation;
+ import Entities.MealPlan;
+public class MealPlanState {
+    private MealPlan mealPlan;
+    private boolean creationSuccess;
+    private String errorMessage;
+    private String startDate;
+    private String endDate;
+    private String diets;
+    private int calorieLimit;
+
+    public MealPlanState() {
+        this.creationSuccess = false;
+        this.errorMessage = "";
+    }
+
+    // Copy constructor
+    public MealPlanState(MealPlanState copy) {
+        this.creationSuccess = copy.creationSuccess;
+        this.errorMessage = copy.errorMessage;
+        this.startDate = copy.startDate;
+        this.endDate = copy.endDate;
+        this.diets = copy.diets;
+        this.calorieLimit = copy.calorieLimit;
+    }
+
+    // Getters and setters
+
+    public MealPlan getMealPlan(){
+        return mealPlan;
+    }
+
+    public void setMealPlan(MealPlan mealPlan) {
+        this.mealPlan = mealPlan;
+    }
+
+    public boolean isCreationSuccess() {
+        return creationSuccess;
+    }
+
+    public void setCreationSuccess(boolean creationSuccess) {
+        this.creationSuccess = creationSuccess;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDiets() {
+        return diets;
+    }
+
+    public void setDiets(String diets) {
+        this.diets = diets;
+    }
+
+    public int getCalorieLimit() {
+        return calorieLimit;
+    }
+
+    public void setCalorieLimit(int calorieLimit) {
+        this.calorieLimit = calorieLimit;
+    }
+
+    // Override toString for debugging purposes
+    @Override
+    public String toString() {
+        return "MealPlanState{" +
+                "startDate='" + startDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                ", diets='" + diets + '\'' + ", calorieLimit='" + calorieLimit + '\'' +
+                '}';
+    }
+}
---
+++ src/InterfaceAdapter/MealPlanCreation/MealPlanViewModel.java
@@ -0,0 +1,40 @@
+package InterfaceAdapter.MealPlanCreation;
+
+
+import Entities.MealPlan;
+import InterfaceAdapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class MealPlanViewModel extends ViewModel {
+    private MealPlanState state = new MealPlanState();
+
+    public MealPlanViewModel() {
+        super("sign up");
+    }
+
+    public void setState(MealPlanState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public MealPlanState getState() {
+        return state;
+    }
+
+    public void updateMealPlanState(MealPlan mealPlan){
+        state.setMealPlan(mealPlan);
+        state.setCreationSuccess(true);
+        firePropertyChanged();
+    }
+}
