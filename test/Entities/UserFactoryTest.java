330912ad861a1068f7d494d89f067bae735f51f5
Yazdan Mohajerin <ymohajerin@gmail.com>
2023-12-02 18:45:36 -0500

    Added test case for user and user factory

--- test/Entities/UserFactoryTest.java
+++ test/Entities/UserFactoryTest.java
@@ -1,4 +1,26 @@
+package Entities;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserFactoryTest {
-  
+
+    @Test
+    void create() {
+        UserFactory factory = new UserFactory();
+        String expectedUserId = "user123";
+        String expectedUsername = "testUser";
+        String expectedPassword = "testPass";
+        LocalDateTime expectedCreationTime = LocalDateTime.now();
+
+        User user = factory.create(expectedUserId, expectedUsername, expectedPassword, expectedCreationTime);
+
+        assertNotNull(user, "User should not be null");
+        assertEquals(expectedUserId, user.getUserId(), "User ID should match");
+        assertEquals(expectedUsername, user.getUsername(), "Username should match");
+        assertEquals(expectedPassword, user.getPassword());
+    }
 }
--- test/Entities/UserTest.java
+++ test/Entities/UserTest.java
@@ -1,4 +1,40 @@
+package Entities;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserTest {
-  
+    private User user;
+    private final String userId = "user123";
+    private final String username = "testUser";
+    private final String password = "testPass";
+    private final LocalDateTime creationTime = LocalDateTime.now();
+    @BeforeEach
+    void setUp() {
+        user = new User(userId, username, password, creationTime);
+    }
+
+    @org.junit.jupiter.api.Test
+    void userPropertiesAreCorrectlyInitialized() {
+        assertEquals(userId, user.getUserId());
+        assertEquals(username, user.getUsername());
+        assertEquals(password, user.getPassword());
+        assertEquals(creationTime, user.getCreationTime());
+        assertTrue(user.getFavoriteRecipes().isEmpty());
+    }
+
+    @org.junit.jupiter.api.Test
+    void addingFavoriteRecipeWorksCorrectly() {
+        String recipe = "Chocolate Cake";
+        user.addFavoriteRecipe(recipe);
+
+        List<String> favoriteRecipes = user.getFavoriteRecipes();
+        assertFalse(favoriteRecipes.isEmpty());
+        assertEquals(1, favoriteRecipes.size());
+        assertTrue(favoriteRecipes.contains(recipe));
+    }
 }
