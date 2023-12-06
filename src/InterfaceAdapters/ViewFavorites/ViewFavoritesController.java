d343d8bb6459ec3c47051bdd67c32bdefecf5df5
Yazdan Mohajerin <ymohajerin@gmail.com>
2023-12-05 23:47:46 -0500

    implemeted the view Favorites interface adapters

--- src/InterfaceAdapters/ViewFavorites/ViewFavoritesController.java
+++ src/InterfaceAdapters/ViewFavorites/ViewFavoritesController.java
@@ -1,2 +1,14 @@
-package InterfaceAdapters.ViewFavorites;public class ViewFavoritesController {
+package InterfaceAdapters.ViewFavorites;
+
+import UseCase.ViewFavorites.ViewFavoritesInputBoundary;
+
+public class ViewFavoritesController {
+    private final ViewFavoritesInputBoundary interactor;
+
+    public ViewFavoritesController(ViewFavoritesInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+    public void execute(){
+        interactor.execute();
+    }
 }
